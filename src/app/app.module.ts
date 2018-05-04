bbed0369089383fb412cfa8af7ab6ffc90db9319
Wolfgang Heindl <Wolfi1985>
2018-05-04 18:32:58 +0200

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+// app-component => root component
 import { AppComponent } from './app.component';
 
+// components
+import { LoginComponent } from './login/components/login.component';
+import { MainComponent } from './main/components/main.component';
+
+// services
+import { MainService } from './main/services/main.service';
+import { LoginService } from './login/services/login.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    MainService,
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
